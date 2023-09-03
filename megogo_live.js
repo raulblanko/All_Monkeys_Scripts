4e603a695f27700b37b48f254fa88f81d45adfc3
Pavlo <a.agency@ukr.net>
2023-09-03 17:50:07 +0300

    megogo 1: red live works

---
+++ megogo_live.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        Megogo Live
+// @namespace   Violentmonkey Scripts
+// @match       https://megogo.net/ua/tv/channels/*
+// @grant       none
+// @version     1.0
+// @author      raulblanko
+// @updateURL   https://raw.githubusercontent.com/raulblanko/All_Monkeys_Scripts/main/megogo_live.js
+// @downloadURL https://raw.githubusercontent.com/raulblanko/All_Monkeys_Scripts/main/megogo_live.js
+// @description 03.09.2023, 15:55:40
+// ==/UserScript==
+console.log('Megogo Live. start');
+function red_live() {
+  spans = document.querySelectorAll('span.sc-iwsKbI');
+  console.log(spans);
+  spans.forEach(element => {
+    console.log(element.innerText);
+    if (element.innerText.indexOf("Пряма трансляція") !== -1) {
+      element.style.color = "red";
+    }
+  });
+};
+
+var button_red = document.createElement('button');
+button_red.textContent = 'LIVE';
+button_red.style.position = 'fixed';
+button_red.style.top = '5px';
+button_red.style.left = '5px';
+document.body.appendChild(button_red);
+
+button_red.onclick = function() {
+  red_live();
+    //   spans = document.querySelectorAll('span.sc-iwsKbI');
+    //   console.log(spans);
+    //   spans.forEach(element => {
+    //   console.log(element.innerText);
+    //   if (element.innerText.indexOf("Пряма трансляція") !== -1) {
+    //     element.style.color = "red";
+    //   }
+    // });
+};
+ //red_live();
+window.onload = function() {
+  red_live();
+};
+
+console.log('Megogo Live. end');
